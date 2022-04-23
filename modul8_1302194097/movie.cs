1d4eb4dc4a987c65c6e05139be85aed084e7e85c
julianikhsan <auliaikhsan413@gmail.com>
2022-04-23 15:13:13 +0700

    menambahkan API Movies

---
+++ modul8_1302194097/movie.cs
@@ -0,0 +1,10 @@
+﻿namespace modul8_1302194097
+{
+    public class movie
+    {
+        private string Title { get; set; }
+        private string Director { get; set; }
+        private List<string> Stars { get; set; }
+        private String Description { get; set; }
+    }
+}
